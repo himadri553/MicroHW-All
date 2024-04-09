7296043d0975d51d35d0249ca07783c9d902eca2
himadri553 <164025459+himadri553@users.noreply.github.com>
2024-04-08 20:03:01 -0400

    Update DelA.ino

--- DelA.ino
+++ DelA.ino
@@ -1,1 +1,26 @@
+// Pin connected to LED
+#define ledPin 42
 
+void setup() {
+  // Set the ledPin as an output
+  Serial.begin(9600);
+}
+
+void PWM_Signnal(float input) {
+  //convert custom number to duty cycle
+  float duty_cycle;
+  if (input >= 0 && input <= 15) {
+    duty_cycle = (input / 15) * 255; // Convert input to percentage of 255
+    Serial.println(duty_cycle);
+  } else {
+    Serial.println("Input number must be between 0 and 15");
+  }
+
+  // run PWM signal
+  analogWrite(ledPin, duty_cycle);
+}
+
+void loop() {
+  PWM_Signnal(7);
+  delay(10);
+}
