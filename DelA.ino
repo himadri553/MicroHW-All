7e19ce452176c7672c9c001356251352517a8f9a
Dan12112 <162073178+Dan12112@users.noreply.github.com>
2024-04-08 20:28:22 -0400

    Update DelA.ino

--- DelA.ino
+++ DelA.ino
@@ -11,7 +11,7 @@ void PWM_Signnal(float input) {
   //convert custom number to duty cycle
   float duty_cycle;
   if (input >= 0 && input <= 15) {
-    duty_cycle = ((input / 15) * 255) + 136; // Convert input to percentage of 255
+    duty_cycle = ((input / 15) * 255); // Convert input to percentage of 255
     Serial.println(duty_cycle);
   } else {
     Serial.println("Input number must be between 0 and 15");
@@ -22,7 +22,7 @@ void PWM_Signnal(float input) {
 }
 
 void loop() {
-  for (int i = 0; i < 15; i) {
+  for (int i = 0; i < 15; i++) {
     PWM_Signnal(i);
     delay(1000);
   }
