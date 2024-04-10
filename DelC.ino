859af99e01e9d9489b3d0ee572cfe8448169b840
Dan12112 <162073178+Dan12112@users.noreply.github.com>
2024-04-10 15:13:32 -0400

    Update DelC.ino

--- DelC.ino
+++ DelC.ino
@@ -1,1 +1,23 @@
+// Define the pin the switch is connected to
+const int switchPin = 2; // Change this to match the pin you've connected the switch to
+
+void setup() {
+  pinMode(switchPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Read the state of the switch
+  int switchState = digitalRead(switchPin);
+
+  // Check if the switch is turned on
+  if (switchState == HIGH) {
+    // switch is on
+  }
+  else
+  { (switchState == LOW) 
+    // switch is off
+  }
+  delay(1000);
+}
 
