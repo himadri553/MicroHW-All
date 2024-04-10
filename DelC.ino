6750e26cb7a4b7650afe4a84647fd1cd58fde552
himadri553 <164025459+himadri553@users.noreply.github.com>
2024-04-10 16:33:16 -0400

    Update DelC.ino

--- DelC.ino
+++ DelC.ino
@@ -1,4 +1,11 @@
-const int switchPin = 2;
+/*
+  Del C:
+  button(toggle) will turn LED on or off
+  once the button is turned on, wait and turn servo on to push the button off
+  if possible, use a counter to make the machine more "mad" and swtich up servo behavior
+*/
+#define buttonPin 2
+#define LED_Pin 42
 
 void setup() {
   pinMode(switchPin, INPUT);
