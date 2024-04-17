1eeef1b4857ab7c939fa94b00393d2bf86e60ae8
himadri553 <164025459+himadri553@users.noreply.github.com>
2024-04-17 18:13:25 -0400

    Update DelB.c

--- DelB.c
+++ DelB.c
@@ -1,1 +1,20 @@
+/*DEL B*/
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#define LED PG5 // blue: dpin 4
 
+void setup() {
+  DDRG |= (1<<LED); // setup led
+}
+
+void mainLoop() {
+  
+}
+
+int main() {
+  setup();
+  while(1){
+    mainLoop();
+  }
+}
