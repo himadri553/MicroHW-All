b51ba278673b2bfeb32c55402d92f120ae6fe325
himadri553 <164025459+himadri553@users.noreply.github.com>
2024-04-17 18:21:30 -0400

    Update DelB.c

--- DelB.c
+++ DelB.c
@@ -1,20 +1,27 @@
-/*DEL B*/
+/*Del B*/
 #include <avr/io.h>
 #include <util/delay.h>
-#include <avr/interrupt.h>
-#define LED PG5 // blue: dpin 4
+
+#define LED PG5 // Blue LED connected to pin 13 (PG5)
 
 void setup() {
-  DDRG |= (1<<LED); // setup led
+  DDRG |= (1 << LED); // Set LED pin to output
 }
 
 void mainLoop() {
-  
+  // Cycle through five levels of brightness (0%, 25%, 50%, 75%, 100%)
+  for (int i = 0; i <= 100; i += 25) {
+    OCR0B = (i * 255) / 100; // Map percentage to PWM value
+    TCCR0A = 0b10100011; // Set fast PWM mode, non-inverting mode
+    TCCR0B = 0b00000001; // Start PWM output
+    _delay_ms(500); // Pause for 500 milliseconds
+  }
 }
 
 int main() {
   setup();
-  while(1){
+  while (1) {
     mainLoop();
   }
+  return 0;
 }
