60633cdb428bf47f997b131eee1033d11a6ee328
himadri553 <164025459+himadri553@users.noreply.github.com>
2024-04-17 18:06:22 -0400

    Update DelD.c

--- DelD.c
+++ DelD.c
@@ -6,28 +6,45 @@
   Set up an interrupt 
   that will swap between B and C above when the button is pressed
 */
+#include <avr/io.h>
+#include <util/delay.h>
 #include <avr/interrupt.h>
-#define LED 5 // AVR pin eq
-#define button 6 // yellow
-#define Pot 7 // green
+
+#define LED PG5 // blue: dpin 4
+#define button PD2 // yellow: dpin 19, INT2 
+#define Pot PINH4 // green: dpin 7
+int DelState = 0; // 0: B, 1: C
 
 void setup() {
-  // Set LED pin to output (pin 4)
-  DDRG |= (1 << LED);
-  
+  DDRG |= (1 << LED); // Set LED pin to output (pin 4)
+  DDRD &= ~(1<<button); // dpin 51 (pullup)
+  PORTD |= (1<<button);
+
+  // enable interrupt
+  EIMSK |= (1<<INT2);
+  EICRA |= (1<<ISC20);
+  sei();
 }
 
 void mainLoop() {
-  OCR0B = 20; // set duty cycle and compare register 
-  TCCR0A = 0b10100011; // set fast mode
-  TCCR0B = 0b00000001; // start PWM output
+  // check which Del to be in, and the run that Del
+  if (DelState == 1) {
+    // run delB
+    PORTG |= (1<<LED);
+  }
+  else {
+    // run delC
+    PORTG &= ~(1 << LED);
+  }
 }
 
-int main(void) {
+int main() {
   setup();
   while(1){
     mainLoop();
   }
+}
 
-  return 0;
+ISR (INT2_vect) {
+  DelState = !DelState;
 }
