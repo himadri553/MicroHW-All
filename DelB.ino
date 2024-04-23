8caef14ff7536f6bd5e88c6bd245b569c2e6e05d
Dan12112 <162073178+Dan12112@users.noreply.github.com>
2024-04-23 16:09:19 -0400

    Update DelB.ino

--- DelB.ino
+++ DelB.ino
@@ -1,1 +1,40 @@
+#include <avr/io.h>
+#include <util/delay.h>
 
+#define LED PG5
+#define POT PF0
+
+void setup() {
+  DDRG |= (1 << LED); // LED setup
+  DDRF &= ~(1 << POT); // Potentiometer setup
+  PORTF &= ~(1 << POT);
+
+  // Configure ADC
+  ADMUX = (1 << REFS0); // Set reference voltage to AVcc
+  ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // Enable ADC, set prescaler to 128
+}
+
+void mainLoop() {
+  // Start ADC conversion and wait for it to complete 
+  ADCSRA |= (1 << ADSC);
+  while (ADCSRA & (1 << ADSC));
+
+  // Get ADC value 
+  unsigned int adcValue = ADC;
+
+  // light LED up
+  OCR0B = ADC/10; // Map percentage to PWM value
+  TCCR0A = 0b10100011; // Set fast PWM mode, non-inverting mode
+  TCCR0B = 0b00000001; // Start PWM output
+
+  // Delay for stability 
+  _delay_ms(10);
+}
+
+int main() {
+  setup();
+  while (1) {
+    mainLoop();
+  }
+  return 0;
+}
