9dd18d8cd4826f649b51c64ae92fb957b1001a2a
himadri553 <164025459+himadri553@users.noreply.github.com>
2024-04-24 21:01:20 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,50 +1,58 @@
-#include <avr/io.h>
-#define LED_PIN PG5
+  // backwards: RServo.write(180); 
+  // forwards: RServo.write(-180); 
+  // forwards: LServo.write(180); 
+  // backwards: LServo.write(-180); 
+  // Stop: LServo.write(90); 
+  // Stop: RServo.write(90);
+#include <Servo.h>
+Servo RServo;
+Servo LServo;
+#define R_ServoPin 44
+#define L_ServoPin 45
 
-.global main
-main:
-; Define I/O port addresses
-BUTTON_PORT equ 0x0001 ; Address of the button input port
-LED_PORT    equ 0x0002 ; Address of the LED output port
+void setup() {
+  Serial.begin(9600);
+  RServo.attach(R_ServoPin);
+  LServo.attach(L_ServoPin);
+}
 
-; Define constants
-BUTTON_MASK equ 0x01   ; Mask for the button input
-LED_MASK    equ 0x02   ; Mask for the LED output
+/*All movement functions: Must have a delay after otherwise it wont stop!*/
+void forwards(int duration) {
+  LServo.write(180); 
+  RServo.write(-180);
+  delay(duration);
+  LServo.write(90); 
+  RServo.write(90);
+}
 
-section .text
-global _start
+void backwards(int duration) {
+  LServo.write(-180); 
+  RServo.write(180);
+  delay(duration);
+  LServo.write(90); 
+  RServo.write(90);
+}
 
-_start:
-    ; Initialize ports
-    mov bx, BUTTON_PORT
-    mov ax, 0xFFFF         ; Set all pins of the button port as inputs
-    out bx, ax
+void turnLeft(int duration) {
+  LServo.write(180); 
+  RServo.write(180);
+  delay(duration);
+  LServo.write(90); 
+  RServo.write(90);
+}
 
-    mov bx, LED_PORT
-    mov ax, 0x0000         ; Set all pins of the LED port as outputs
-    out bx, ax
+void turnRight(int duration) {
+  LServo.write(-180); 
+  RServo.write(-180);
+  delay(duration);
+  LServo.write(90); 
+  RServo.write(90);
+}
 
-poll_button:
-    ; Read the state of the button
-    mov bx, BUTTON_PORT
-    in ax, bx
-    test ax, BUTTON_MASK   ; Check if the button is pressed
-    jnz turn_on_led        ; If the button is pressed, jump to turn_on_led
-    jmp turn_off_led       ; Otherwise, jump to turn_off_led
-
-turn_on_led:
-    ; Turn on the LED
-    mov bx, LED_PORT
-    in ax, bx
-    or ax, LED_MASK        ; Set the LED pin to high
-    out bx, ax
-    jmp poll_button        ; Go back to polling the button
-
-turn_off_led:
-    ; Turn off the LED
-    mov bx, LED_PORT
-    in ax, bx
-    and ax, not LED_MASK   ; Set the LED pin to low
-    out bx, ax
-    jmp poll_button        ; Go back to polling the button
-
+void loop() {
+  forwards(2000);
+  turnRight(2000);
+  backwards(2000);
+  turnLeft(2000);
+  delay(2000);
+}
