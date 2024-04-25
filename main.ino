1d62b78b36213fcb780d4899e9404c7f8942d454
himadri553 <164025459+himadri553@users.noreply.github.com>
2024-04-25 12:02:59 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -4,16 +4,22 @@
   // backwards: LServo.write(-180); 
   // Stop: LServo.write(90); 
   // Stop: RServo.write(90);
+  // IR d46
 #include <Servo.h>
+#include <IRremote.hpp> 
 Servo RServo;
 Servo LServo;
 #define R_ServoPin 44
 #define L_ServoPin 45
+#define IR_Reciver 46
+int debouceFactor = 50;
+int lastButtonPress = 0;
 
 void setup() {
   Serial.begin(9600);
   RServo.attach(R_ServoPin);
   LServo.attach(L_ServoPin);
+  IrReceiver.begin(IR_Reciver, ENABLE_LED_FEEDBACK);
 }
 
 /*All movement functions: Must have a delay after otherwise it wont stop!*/
@@ -50,9 +56,30 @@ void turnRight(int duration) {
 }
 
 void loop() {
-  forwards(2000);
-  turnRight(2000);
-  backwards(2000);
-  turnLeft(2000);
-  delay(2000);
+  if (IrReceiver.decode()) { // check for button press
+    if (millis() >= (lastButtonPress + debouceFactor)) { // check for debounce
+      lastButtonPress = millis();
+      switch (IrReceiver.decodedIRData.command) {
+        case 0x46: //forwards
+          forwards(1000);
+          delay(1000);
+        break;
+
+        case 0x15: // down
+          backwards(1000);
+          delay(1000);
+        break;
+
+        case 0x44:// left
+          turnLeft(1000);
+          delay(1000);
+        break;
+
+        case 0x43: // right
+          turnRight(1000);
+          delay(1000);
+        break;
+      }
+    }
+  }
 }
