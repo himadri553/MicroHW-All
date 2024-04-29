32d3dcd9686b54542ee0867d3ef9e73af025d4ff
himadri553 <164025459+himadri553@users.noreply.github.com>
2024-04-29 13:41:19 -0400

    Update Deliverable_B.ino

--- Deliverable_B.ino
+++ Deliverable_B.ino
@@ -1,3 +1,9 @@
+/* 
+  corrected:
+  - pin definitions 
+  - changed potiometer to anlogRead
+
+*/
 #define RED_LED 3
 #define Speaker 8
 #define button 2
@@ -7,27 +13,27 @@ void setup() {
   pinMode(RED_LED, OUTPUT);
   pinMode(Speaker, OUTPUT);
   pinMode(button, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(button),buttonPress,FALLING);
+  attachInterrupt(digitalPinToInterrupt(button), buttonPress, FALLING);
 }
 
-void loop() {
-  digitalWrite(RED_LED,HIGH);
-  int aval = digitalRead(A0);
-  if(mode) {
+void loop() { // Based on mode, play tone on speaker
+  digitalWrite(RED_LED, HIGH);
+  int aval = analogRead(A0);
+  if (mode) {
     tone(Speaker, 4978-(4*aval),1);
   }
   else {
-    analogWrite(RED_LED,aval);
+    analogWrite(RED_LED, aval);
   }
 }
 
-void buttonPress() {
-  if(mode) {
+void buttonPress() { // swtich modes at interupt, 
+  if (mode) {
     noTone(Speaker);
     mode = 0;
   }
   else {
-    analogWrite(RED_LED,0);
+    analogWrite(RED_LED, 0);
     mode = 1;
   }
 }
