a007eefe3ec02e8fbd5328d5d905d1bad0a15c3e
himadri553 <164025459+himadri553@users.noreply.github.com>
2024-04-29 13:20:56 -0400

    Update Deliverable_A.ino

--- Deliverable_A.ino
+++ Deliverable_A.ino
@@ -1,12 +1,12 @@
-#define LED_Pin 6 // define pin number for easy acess
+#define LED_Pin 5 // define pin number for easy acess
 
 void setup() {
-  pinMode(LED_Pin,OUTPUT);
+  pinMode(LED_Pin, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_Pin,HIGH);
-  delay(250);
-  digitalWrite(LED_Pin,LOW);
-  delay(250);
+  digitalWrite(LED_Pin, HIGH);
+  delay(100);
+  digitalWrite(LED_Pin, LOW);
+  delay(100);
 }
