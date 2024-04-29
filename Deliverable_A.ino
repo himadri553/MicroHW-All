cf54ef1949b7893b83236f7b1a06b60b148270a4
himadri553 <164025459+himadri553@users.noreply.github.com>
2024-04-29 13:31:27 -0400

    Update Deliverable_A.ino

--- Deliverable_A.ino
+++ Deliverable_A.ino
@@ -1,3 +1,10 @@
+/*
+  corrected: 
+  - pin definitons
+  - delay value by using:
+    freq = 1 / cycle length (seconds), and solving for delay value given freq = 5Hz. 
+    Led is on for only half a cycle, so delay = cycle length / 2. Then converted into ms
+*/
 #define LED_Pin 5 // define pin number for easy acess
 
 void setup() {
